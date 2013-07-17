04a5f96a3500b031103e75e7a27f2c7aebcfaa06
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2013-07-17 21:32:51 +0200

    Formatting

--- src/main/java/com/blogspot/nurkiewicz/asyncretry/policy/exception/AbortRetryException.java
+++ src/main/java/com/blogspot/nurkiewicz/asyncretry/policy/exception/AbortRetryException.java
@@ -4,7 +4,7 @@ package com.blogspot.nurkiewicz.asyncretry.policy.exception;
  * @author Tomasz Nurkiewicz
  * @since 7/16/13, 10:23 PM
  */
-public class AbortRetryException extends  RuntimeException {
+public class AbortRetryException extends RuntimeException {
 
 	public AbortRetryException() {
 	}
